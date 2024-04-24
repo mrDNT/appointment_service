b5b887658d6565473d9aaaa29b0228dcfb4607c5
morodont <40578464+mrDNT@users.noreply.github.com>
2024-04-25 02:28:14 +0300

    change tests

--- test_appointment_repo.py
+++ test_appointment_repo.py
@@ -1,93 +1,42 @@
 import pytest
 from uuid import uuid4
-from time import sleep
 from datetime import datetime
-
+from pydantic import ValidationError
+from app.models.trainers import Trainer
 from app.models.appointments import Appointments, AppointmentStatuses
-from app.repositories.db_appointments_repo import AppointmentsRepo
-from app.repositories.trainers_repo import TrainersRepo
-from app.settings import settings
-
-sleep(5)
-
 
 @pytest.fixture()
-def appointment_repo() -> AppointmentsRepo:
-    repo = AppointmentsRepo()
-    return repo
+def any_trainer() -> Trainer:
+    return Trainer(id=uuid4(), name='trainer')
 
+def test_appointment_creation(any_trainer: Trainer):
+    id = uuid4()
+    name = 'name'
+    date = datetime.now()
+    status = AppointmentStatuses.DONE
+    appointment = Appointments(id=id, name=name, creation_date=date, 
+                               appointment_date=date,status=status, trainer=any_trainer)
 
-@pytest.fixture(scope='session')
-def trainers_repo() -> TrainersRepo:
-    return TrainersRepo()
+    assert dict(appointment) == {'id': id, 'name': name, 'creation_date': date,
+                                 'appointment_date': date, 'status': status,
+                                 'trainer': any_trainer}
 
+def test_appointment_type_required(any_trainer: Trainer):
+    with pytest.raises(ValidationError):
+        Appointments(id=uuid4(), date=datetime.now(),
+                 status=AppointmentStatuses.ACTIVATED, trainer=any_trainer)
 
-@pytest.fixture(scope='session')
-def first_appointment() -> Appointments:
-    return Appointments(id=uuid4(), type='type', creation_date=datetime.now(),appointment_date=datetime.now(), status=AppointmentStatuses.CREATED)
+def test_appointment_appointment_date_required(any_trainer: Trainer):
+    with pytest.raises(ValidationError):
+        Appointments(id=uuid4(), name='str', creation_date = datetime.now(),
+                 status=AppointmentStatuses.ACTIVATED, trainer=any_trainer)
+        
+def test_appointment_creation_date_required(any_trainer: Trainer):
+    with pytest.raises(ValidationError):
+        Appointments(id=uuid4(), name='str', appointment_date = datetime.now(),
+                 status=AppointmentStatuses.ACTIVATED, trainer=any_trainer)
 
-
-@pytest.fixture(scope='session')
-def second_appointment() -> Appointments:
-    return Appointments(id=uuid4(), type='type', creation_date=datetime.now(),appointment_date=datetime.now(), status=AppointmentStatuses.CREATED)
-
-
-def test_empty_list(appointment_repo: AppointmentsRepo) -> None:
-    assert appointment_repo.get_appointment() == []
-
-
-def test_add_first_appointment(first_appointment: Appointments, appointment_repo: AppointmentsRepo) -> None:
-    assert appointment_repo.create_appointment(first_appointment) == first_appointment
-
-
-def test_add_first_appointment_repeat(first_appointment: Appointments, appointment_repo: AppointmentsRepo) -> None:
-    with pytest.raises(KeyError):
-        appointment_repo.create_appointment(first_appointment)
-
-
-def test_get_appointment_by_id(first_appointment: Appointments, appointment_repo: AppointmentsRepo) -> None:
-    assert appointment_repo.get_appointment_by_id(
-        first_appointment.id) == first_appointment
-
-
-def test_get_appointment_by_id_error(appointment_repo: AppointmentsRepo) -> None:
-    with pytest.raises(KeyError):
-        appointment_repo.get_appointment_by_id(uuid4())
-
-
-def test_add_second_appointment(first_appointment: Appointments, second_appointment: Appointments, appointment_repo: AppointmentsRepo) -> None:
-    assert appointment_repo.create_appointment(second_appointment) == second_appointment
-    appointments = appointment_repo.get_appointment()
-    assert len(appointments) == 2
-    assert appointments[0] == first_appointment
-    assert appointments[1] == second_appointment
-
-
-def test_set_status(first_appointment: Appointments, appointment_repo: AppointmentsRepo) -> None:
-    first_appointment.status = AppointmentStatuses.ACTIVATED
-    assert appointment_repo.set_status(
-        first_appointment).status == first_appointment.status
-
-    first_appointment.status = AppointmentStatuses.CANCELED
-    assert appointment_repo.set_status(
-        first_appointment).status == first_appointment.status
-
-    first_appointment.status = AppointmentStatuses.DONE
-    assert appointment_repo.set_status(
-        first_appointment).status == first_appointment.status
-
-    first_appointment.status = AppointmentStatuses.CREATED
-    assert appointment_repo.set_status(
-        first_appointment).status == first_appointment.status
-
-
-def test_set_trainers(first_appointment: Appointments, trainers_repo: TrainersRepo, appointment_repo: AppointmentsRepo) -> None:
-    first_appointment.trainer = trainers_repo.get_trainer()[0]
-    assert appointment_repo.set_trainer(
-        first_appointment).trainer == trainers_repo.get_trainer()[0]
-
-
-def test_change_trainers(first_appointment: Appointments, trainers_repo: TrainersRepo, appointment_repo: AppointmentsRepo) -> None:
-    first_appointment.trainer = trainers_repo.get_trainer()[1]
-    assert appointment_repo.set_trainer(
-        first_appointment).trainer == trainers_repo.get_trainer()[1]
+def test_appointment_status_required(any_trainer: Trainer):
+    with pytest.raises(ValidationError):
+        Appointments(id=uuid4(), date=datetime.now(),
+                 name='str', trainer=any_trainer)
