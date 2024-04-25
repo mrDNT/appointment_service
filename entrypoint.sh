3c83d5e98220e2f9b43fdf1bcffb3ab7d41625f9
morodont <40578464+mrDNT@users.noreply.github.com>
2024-04-25 09:56:36 +0300

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,3 @@
 
 alembic revision --autogenerate -m 'initial'
 alembic upgrade head
-uvicorn main:app --host 0.0.0.0 --port 80
--- main.py
+++ main.py
@@ -6,13 +6,11 @@ app = FastAPI(title='Appointment Service')
 
 app.include_router(appointment_router, prefix='/api')
 
-"""
 if __name__ == '__main__':
     import uvicorn
     import os
     try:
-        PORT = int(os.environ['PORT'])
+        PORT = int(os.getenv['PORT',80])
     except KeyError as keyerr:
         PORT = 80
     uvicorn.run(app, host='0.0.0.0', port=PORT)
-"""
