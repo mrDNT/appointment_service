4ef976189bdf746c16c64ed962b2de2b67dd9f7d
morodont <40578464+mrDNT@users.noreply.github.com>
2024-04-25 10:19:41 +0300

    test

--- main.py
+++ main.py
@@ -5,12 +5,3 @@ from app.endpoints.appointment_router import appointment_router
 app = FastAPI(title='Appointment Service')
 
 app.include_router(appointment_router, prefix='/api')
-
-if __name__ == '__main__':
-    import uvicorn
-    import os
-    try:
-        PORT = int(os.getenv['PORT',80])
-    except KeyError as keyerr:
-        PORT = 80
-    uvicorn.run(app, host='0.0.0.0', port=PORT)
